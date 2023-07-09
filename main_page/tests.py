7d5c863bce4da45e4ea956a49858cdade3f1cb46
egor <goldenbereg97@gmail.com>
2023-07-10 01:31:55 +0300

    new

--- main_page/tests.py
+++ main_page/tests.py
@@ -1,14 +1,1 @@
 from django.test import TestCase
-
-
-class TestViews(TestCase):
-
-    def test_index(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('On this page you can explore our rich collection of wall materials',
-                      response.content.decode())
-
-    def test_materials(self):
-        response = self.client.get('/api/wall/materials/')
-        self.assertEqual(response.status_code, 200)
