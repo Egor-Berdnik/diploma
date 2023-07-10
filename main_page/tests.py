7b9f35f35db77a79df35112c6312b9c80e2949b7
egor <goldenbereg97@gmail.com>
2023-07-10 11:54:40 +0300

    coerrection

--- main_page/tests.py
+++ main_page/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from .models import Producers
