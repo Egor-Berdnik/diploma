5ac515c435ed5590b1e88322cdeabeb677ad251c
egor <goldenbereg97@gmail.com>
2023-07-18 12:27:39 +0300

    corrected

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,6 @@
 from django.http import QueryDict
 from django.shortcuts import render
-from rest_framework.generics import APIView
+from rest_framework.views import APIView
 from rest_framework import generics, viewsets, status
 from rest_framework.renderers import TemplateHTMLRenderer
 from django.shortcuts import get_object_or_404
